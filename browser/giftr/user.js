fe2a4bb0aeeb8772c94826bd208074c651ca8e31
andrewhatran <andrewhatran@wifi-131-179-46-95.host.ucla.edu>
2016-05-05 00:55:37 -0700

    Add json parse and catalog
    Added son parsing and catalog

---
+++ browser/giftr/user.js
@@ -0,0 +1,19 @@
+var text = {
+  "id": 23,
+  "status": {
+    "id": 1,
+    "name": "Active"
+  },
+  "joinDate": "2016-05-02",
+  "lastActive": "2016-05-02",
+  "gender": {
+    "id": 1,
+    "name": "Male"
+  },
+  "location": "Jamaica",
+  "giftType": {
+    "id": 1,
+    "name": "Thrifty"
+  },
+  "interests": "Video Games"
+}
